36c207e7a6b176c5a216c3270091a4f824f57daa
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-05-12 15:57:56 +0900

    title設定

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-$(function(){ 
-  $('.fa-bicycle').click(function(){
-    $('.dropdown').fadeToggle();
-    // $(this).css('display','none');
+$(function() {
+    $("#panel-btn").click(function() {
+      $("#panel").slideToggle(200);
+      $("#panel-btn-icon").toggleClass("close");
+      $('.dropdown').fadeToggle(200)
+      return false;
+    });
   });
-});
+  
