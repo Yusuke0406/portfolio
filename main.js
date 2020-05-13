b113ceeb40f21ae482ad8c59173bf8adbecb0f6d
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-05-13 14:54:19 +0900

    skillbar追加

--- main.js
+++ main.js
@@ -5,5 +5,25 @@ $(function() {
       $('.dropdown').fadeToggle(200)
       return false;
     });
+
+
+    var mvh = $('.page1').height();
+    $(window).scroll(function() {
+      var top = $(window).scrollTop();
+      if (mvh <= top) {
+        $('body').css('background-color', ' #437076');
+        $('.page2').css('color','Window')
+        $('.skillbar').each(function(){
+        $(this).find('.skillbar-bar').animate({
+            width:$(this).attr('data-percent')
+          },4000);
+        });
+        } else {
+        $('body').css('background-color', '#efeff0');
+        $('.page2').css('color','black')
+      }
+    });
   });
+
+  
   
